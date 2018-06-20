c53a14c3442cd969a3b30c22b835f11a00e4c901
Yu Jin <tenstar718@gmail.com>
2018-06-21 00:53:37 +0800

    Update Restframework

--- Backlays/backend/urls.py
+++ Backlays/backend/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+
 
 urlpatterns = [
     path('',views.index, name='index'),
     path('test',views.test, name='sss'),
+    path('getSports',views.SportList.as_view())
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- Backlays/backend/views.py
+++ Backlays/backend/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Sport
+from .serializers import SportSerializer
 
 # Create your views here.
+
+class SportList(APIView):
+    def get (self, request):
+        sports = Sport.objects.all()
+        serializer = SportSerializer(sports,many = True)
+        return Response(serializer.data)
+    def post (self, request):
+        sports = Sport.objects.all()
+        serializer = SportSerializer(sports,many = True)
+        return Response(serializer.data)
 
 def index(request):
     return HttpResponse("<h1>test</h1>")
