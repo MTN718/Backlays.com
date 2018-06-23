b817c07b80d861c7bea5e72186f3cd227b011a61
Yu Jin <tenstar718@gmail.com>
2018-06-23 10:00:30 +0800

    Update

--- Backlays/backend/urls.py
+++ Backlays/backend/urls.py
@@ -5,9 +5,15 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
-    path('',views.index, name='index'),
-    path('test',views.test, name='sss'),
-    path('getSports',views.SportList.as_view())
+    # path('',views.index),
+    # path('dashboard',views.dashboard),
+    # path('systemadmin',views.systemAdmin),
+    # path('login',views.login),
+    # path('actionLogin',views.actionLogin),
+    # path('actionLogout',views.actionLogout),
+    # path('test',views.test, name='sss'),
+    path('getSports',views.SportList.as_view()),
+    path('getCompetitions',views.CompetitionList.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
